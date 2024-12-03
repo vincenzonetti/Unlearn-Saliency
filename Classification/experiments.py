276a37e3d5b138d166ca46fcc114e4e0e91312b3
Vincenzo <vincenzontt02@gmail.com>
2024-12-03 10:42:47 +0100

    Mmod

--- Classification/experiments.py
+++ Classification/experiments.py
@@ -14,23 +14,31 @@ mask_path = "masks"
 #subprocess.run(f"mkdir {unlearn_path}", shell=True, check=True)
 json_file_path = 'indexes_to_replace42.json'
 tenXCifar10 = 4500
-forgetting_data_amount = [tenXCifar10] 
+forgetting_data_amount = [tenXCifar10]
 
-for i in range (0,10):
-    print("class:",i)
-    for lr in [0.0001, 0.001, 0.01, 0.1]:
-        command = f"python main_forget.py --unlearn GA --unlearn_epochs 10 --unlearn_lr {lr} --class_to_replace {i} --model_path {model_path} --save_dir {unlearn_path} --mask_path masks/cifar10_class{i}_mask_0.8.pt --random_prune"
+indexes = json_read(json_file_path)
+indexes = json.dumps(indexes)
+command = f"python generate_mask.py --model_path {model_path} --save_dir {mask_path} --indexes_to_replace '{indexes}' --random_prune"
+
+mask_path = "masks"
+
+for lr in [0.0001, 0.001, 0.01]:
+    print(f"Learning rate: {lr}")
+    for i in range(0, 10):
+        print(f"Class: {i}")
+        command = f"python main_forget.py --unlearn GA --unlearn_epochs 10 --unlearn_lr {lr} --class_to_replace {i} --model_path {model_path} --save_dir {unlearn_path} --mask_path masks/cifar10_class{i}_mask_0.5.pt --random_prune"
         subprocess.run(command, shell=True, check=True)
-        command = f"python main_random.py --unlearn GA --unlearn_epochs 10 --unlearn_lr {lr} --class_to_replace {i} --model_path {model_path} --save_dir {unlearn_path} --mask_path masks/cifar10_class{i}_mask_0.8.pt --random_prune"
+        command = f"python main_random.py --unlearn GA --unlearn_epochs 10 --unlearn_lr {lr} --class_to_replace {i} --model_path {model_path} --save_dir {unlearn_path} --mask_path masks/cifar10_class{i}_mask_0.5.pt --random_prune"
         subprocess.run(command, shell=True, check=True)
     
 
-for i in range (0,10):
-    print("class:",i)
-    for lr in [0.0001, 0.001, 0.01, 0.1]:
-        command = f"python main_forget.py --unlearn RL --unlearn_epochs 10 --unlearn_lr {lr} --class_to_replace {i} --model_path {model_path} --save_dir {unlearn_path} --mask_path masks/cifar10_class{i}_mask_0.8.pt --random_prune"
+for lr in [0.0001, 0.001, 0.01, 0.1]:
+    print(f"Learning rate: {lr}")
+    for i in range(0, 10):
+        print(f"Class: {i}")
+        command = f"python main_forget.py --unlearn RL --unlearn_epochs 10 --unlearn_lr {lr} --class_to_replace {i} --model_path {model_path} --save_dir {unlearn_path} --mask_path masks/cifar10_class{i}_mask_0.5.pt --random_prune"
         subprocess.run(command, shell=True, check=True)
-        command = f"python main_random.py --unlearn RL --unlearn_epochs 10 --unlearn_lr {lr} --class_to_replace {i} --model_path {model_path} --save_dir {unlearn_path} --mask_path masks/cifar10_class{i}_mask_0.8.pt --random_prune"
+        command = f"python main_random.py --unlearn RL --unlearn_epochs 10 --unlearn_lr {lr} --class_to_replace {i} --model_path {model_path} --save_dir {unlearn_path} --mask_path masks/cifar10_class{i}_mask_0.5.pt --random_prune"
         subprocess.run(command, shell=True, check=True)
 
 
